7d305d2d44b6dab2f5b80d21ce370fa20186e099
pkid1729 <pkid1729@gmail.com>
2018-09-08 01:11:03 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime a = DateTime.Now;
+            Console.WriteLine(a.ToString());
+            Console.ReadLine();
+        }
+    }
+}
